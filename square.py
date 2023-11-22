fe8ad526df49cb48f8b6f2a2af974efbfc635fd1
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-11-22 11:05:00 -0800

    Added notes to turtle.forward and turtle.right/turtle.left arguments

--- square.py
+++ square.py
@@ -1,6 +1,8 @@
 import turtle
 
+# forward takes an argument of the distance in px to travel
 turtle.forward(50)
+# right/left take an argument of the degrees of an angle to turn
 turtle.right(90)
 
 turtle.forward(50)
