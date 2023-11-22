8c76249f556114684b56fab9f14a1a1e9d1d68dd
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-11-22 12:06:23 -0800

    Completed repeated steps to create 4 planets in total of differing sizes and colors

--- solarsystem.py
+++ solarsystem.py
@@ -9,7 +9,7 @@ turtle.color("orange")
 # begin fill
 turtle.begin_fill()
 # draw circle
-turtle.circle(100)
+turtle.circle(60)
 # end fill
 turtle.end_fill()
 
@@ -21,30 +21,48 @@ turtle.forward(100)
 turtle.pendown()
 
 # set turtle color to gray
+turtle.color("gray")
 
 # begin fill
+turtle.begin_fill()
 # draw circle
+turtle.circle(20)
 # end fill
+turtle.end_fill()
 
 # lift pen up
+turtle.penup()
 # move forwards
+turtle.forward(100)
 # put pen down
+turtle.pendown()
 
 # set turtle color to red
+turtle.color("red")
 
 # begin fill
+turtle.begin_fill()
 # draw circle
+turtle.circle(40)
 # end fill
+turtle.end_fill()
 
 # lift pen up
+turtle.penup()
 # move forwards
+turtle.forward(100)
 # put pen down
+turtle.pendown()
 
-# set turtle color to red
+# set turtle color to green
+turtle.color("green")
 
 # begin fill
+turtle.begin_fill()
 # draw circle
+turtle.circle(30)
 # end fill
+turtle.end_fill()
 
 # end
 
