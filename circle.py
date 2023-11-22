27f47b1171a213fe3cd6d89aee663240c9d6a952
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-11-22 11:08:03 -0800

    Used turtle.circle to draw and circle and added a note of the argument to circle.py

---
+++ circle.py
@@ -0,0 +1,6 @@
+import turtle
+
+# circle takes an argument of the radius of the circle to draw
+turtle.circle(50)
+
+turtle.done()
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import turtle
 
+turtle.circle(50)
 turtle.forward(100)
+turtle.circle(50)
 
 turtle.done()
