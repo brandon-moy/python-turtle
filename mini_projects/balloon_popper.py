7d9e8b8e800e3cfe3b5ec7989b265f3eeb1b6664
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-11-23 19:42:08 -0800

    Created description and plan for balloon popper mini project

---
+++ mini_projects/balloon_popper.py
@@ -0,0 +1,13 @@
+# Mini project to create an object that will grow until it pops when up key is pressed
+# 1. be able to detect inputs
+# 2. require multiple inputs to pop the balloon
+# 3. utilize variables, conditions and function
+
+# start
+# draw the balloon
+# has key been pressed?
+# yes -> has the balloon reach max size?
+#   yes -> clear the balloon and write POP!
+#   no -> increase balloon size and redraw balloon
+# end
+
