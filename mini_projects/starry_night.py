947f086a0cc004fc079cfdb9ec5271eaa4aa771d
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-11-28 10:07:24 -0800

    Created loop to run draw_star function 100 times and utilized windows width and height as constraints

--- mini_projects/starry_night.py
+++ mini_projects/starry_night.py
@@ -8,15 +8,24 @@
 from turtle import *
 from random import *
 
+speed(0)
+
 bgcolor("black")
 hideturtle()
 
+width = window_width()
+height = window_height()
+
 def draw_star(xpos, ypos):
+  size = randrange(2, 7)
   penup()
   goto(xpos, ypos)
   pendown()
-  dot(20, "white")
+  dot(size, "white")
   
-draw_star(0, 0)
+for x in range(100):
+  xpos = randrange((-width / 2), (width / 2))
+  ypos = randrange((-height / 2), ( height / 2))
+  draw_star(xpos, ypos)
 
 done()
